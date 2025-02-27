ccb281313d96e05c262d668ebd036483b227010c
Wojciech Malota-Wojcik <wojtek@exw.co>
2025-02-27 08:51:05 +0100

    Implement operations on varuint64

---
+++ varuint64.go
@@ -0,0 +1,218 @@
+package varuint64
+
+const (
+	// MaxSize is the maximum possible size of varuint64.
+	MaxSize = 9
+
+	followMask = 0x80
+	numberMask = followMask - 1
+	shiftBits  = 7
+)
+
+const (
+	max1Byte = 1<<((iota+1)*shiftBits) - 1
+	max2Bytes
+	max3Bytes
+	max4Bytes
+	max5Bytes
+	max6Bytes
+	max7Bytes
+	max8Bytes
+)
+
+// Size computes the size of the varuint64.
+func Size(v uint64) uint64 {
+	// For last byte last bit is used for data, not for continuation flag.
+	// That's why we may fit 64-bit number in 9 bytes, not 10.
+
+	switch {
+	case v <= max1Byte:
+		return 1
+	case v <= max2Bytes:
+		return 2
+	case v <= max3Bytes:
+		return 3
+	case v <= max4Bytes:
+		return 4
+	case v <= max5Bytes:
+		return 5
+	case v <= max6Bytes:
+		return 6
+	case v <= max7Bytes:
+		return 7
+	case v <= max8Bytes:
+		return 8
+	default:
+		return 9
+	}
+}
+
+// Contains returns true if slice contains complete varuint64.
+func Contains(b []byte) bool {
+	if len(b) == 0 {
+		return false
+	}
+	if len(b) >= MaxSize {
+		return true
+	}
+	for _, v := range b {
+		if v&followMask == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+// Put puts varuint64 in the buffer.
+func Put(b []byte, v uint64) uint64 {
+	// For last byte last bit is used for data, not for continuation flag.
+	// That's why we may fit 64-bit number in 9 bytes, not 10.
+
+	switch {
+	case v <= max1Byte:
+		b[0] = byte(v)
+		return 1
+	case v <= max2Bytes:
+		b[0] = byte(v) | followMask
+		v >>= shiftBits
+		b[1] = byte(v)
+		return 2
+	case v <= max3Bytes:
+		b[0] = byte(v) | followMask
+		v >>= shiftBits
+		b[1] = byte(v) | followMask
+		v >>= shiftBits
+		b[2] = byte(v)
+		return 3
+	case v <= max4Bytes:
+		b[0] = byte(v) | followMask
+		v >>= shiftBits
+		b[1] = byte(v) | followMask
+		v >>= shiftBits
+		b[2] = byte(v) | followMask
+		v >>= shiftBits
+		b[3] = byte(v)
+		return 4
+	case v <= max5Bytes:
+		b[0] = byte(v) | followMask
+		v >>= shiftBits
+		b[1] = byte(v) | followMask
+		v >>= shiftBits
+		b[2] = byte(v) | followMask
+		v >>= shiftBits
+		b[3] = byte(v) | followMask
+		v >>= shiftBits
+		b[4] = byte(v)
+		return 5
+	case v <= max6Bytes:
+		b[0] = byte(v) | followMask
+		v >>= shiftBits
+		b[1] = byte(v) | followMask
+		v >>= shiftBits
+		b[2] = byte(v) | followMask
+		v >>= shiftBits
+		b[3] = byte(v) | followMask
+		v >>= shiftBits
+		b[4] = byte(v) | followMask
+		v >>= shiftBits
+		b[5] = byte(v)
+		return 6
+	case v <= max7Bytes:
+		b[0] = byte(v) | followMask
+		v >>= shiftBits
+		b[1] = byte(v) | followMask
+		v >>= shiftBits
+		b[2] = byte(v) | followMask
+		v >>= shiftBits
+		b[3] = byte(v) | followMask
+		v >>= shiftBits
+		b[4] = byte(v) | followMask
+		v >>= shiftBits
+		b[5] = byte(v) | followMask
+		v >>= shiftBits
+		b[6] = byte(v)
+		return 7
+	case v <= max8Bytes:
+		b[0] = byte(v) | followMask
+		v >>= shiftBits
+		b[1] = byte(v) | followMask
+		v >>= shiftBits
+		b[2] = byte(v) | followMask
+		v >>= shiftBits
+		b[3] = byte(v) | followMask
+		v >>= shiftBits
+		b[4] = byte(v) | followMask
+		v >>= shiftBits
+		b[5] = byte(v) | followMask
+		v >>= shiftBits
+		b[6] = byte(v) | followMask
+		v >>= shiftBits
+		b[7] = byte(v)
+		return 8
+	default:
+		b[0] = byte(v) | followMask
+		v >>= shiftBits
+		b[1] = byte(v) | followMask
+		v >>= shiftBits
+		b[2] = byte(v) | followMask
+		v >>= shiftBits
+		b[3] = byte(v) | followMask
+		v >>= shiftBits
+		b[4] = byte(v) | followMask
+		v >>= shiftBits
+		b[5] = byte(v) | followMask
+		v >>= shiftBits
+		b[6] = byte(v) | followMask
+		v >>= shiftBits
+		b[7] = byte(v) | followMask
+		v >>= shiftBits
+		b[8] = byte(v)
+		return 9
+	}
+}
+
+// Parse parses varuint64 present in the slice.
+func Parse(b []byte) (uint64, uint64) {
+	v := uint64(b[0] & numberMask)
+	if b[0]&followMask == 0 {
+		return v, 1
+	}
+
+	v |= uint64(b[1]&numberMask) << shiftBits
+	if b[1]&followMask == 0 {
+		return v, 2
+	}
+
+	v |= uint64(b[2]&numberMask) << (2 * shiftBits)
+	if b[2]&followMask == 0 {
+		return v, 3
+	}
+
+	v |= uint64(b[3]&numberMask) << (3 * shiftBits)
+	if b[3]&followMask == 0 {
+		return v, 4
+	}
+
+	v |= uint64(b[4]&numberMask) << (4 * shiftBits)
+	if b[4]&followMask == 0 {
+		return v, 5
+	}
+
+	v |= uint64(b[5]&numberMask) << (5 * shiftBits)
+	if b[5]&followMask == 0 {
+		return v, 6
+	}
+
+	v |= uint64(b[6]&numberMask) << (6 * shiftBits)
+	if b[6]&followMask == 0 {
+		return v, 7
+	}
+
+	v |= uint64(b[7]&numberMask) << (7 * shiftBits)
+	if b[7]&followMask == 0 {
+		return v, 8
+	}
+
+	v |= uint64(b[8]) << (8 * shiftBits)
+	return v, 9
+}
---
+++ varuint64_test.go
@@ -0,0 +1,222 @@
+package varuint64
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSize(t *testing.T) {
+	requireT := require.New(t)
+
+	requireT.EqualValues(1, Size(0))
+	requireT.EqualValues(1, Size(1))
+	requireT.EqualValues(1, Size(max1Byte))
+	requireT.EqualValues(2, Size(max1Byte+1))
+	requireT.EqualValues(2, Size(max2Bytes))
+	requireT.EqualValues(3, Size(max2Bytes+1))
+	requireT.EqualValues(3, Size(max3Bytes))
+	requireT.EqualValues(4, Size(max3Bytes+1))
+	requireT.EqualValues(4, Size(max4Bytes))
+	requireT.EqualValues(5, Size(max4Bytes+1))
+	requireT.EqualValues(5, Size(max5Bytes))
+	requireT.EqualValues(6, Size(max5Bytes+1))
+	requireT.EqualValues(6, Size(max6Bytes))
+	requireT.EqualValues(7, Size(max6Bytes+1))
+	requireT.EqualValues(7, Size(max7Bytes))
+	requireT.EqualValues(8, Size(max7Bytes+1))
+	requireT.EqualValues(8, Size(max8Bytes))
+	requireT.EqualValues(9, Size(max8Bytes+1))
+	requireT.EqualValues(9, Size(math.MaxUint64))
+}
+
+func TestContains(t *testing.T) {
+	requireT := require.New(t)
+
+	requireT.False(Contains([]byte{}))
+	requireT.True(Contains([]byte{0x00}))
+	requireT.True(Contains([]byte{0x00, 0x00}))
+	requireT.True(Contains([]byte{0x00, 0x80}))
+	requireT.True(Contains([]byte{0x00, 0xff}))
+	requireT.True(Contains([]byte{0x7F, 0x7F}))
+	requireT.True(Contains([]byte{0x7F, 0x80}))
+	requireT.False(Contains([]byte{0x80}))
+	requireT.False(Contains([]byte{0x80, 0x80}))
+	requireT.False(Contains([]byte{0x80, 0x80, 0x80}))
+	requireT.False(Contains([]byte{0x80, 0x80, 0x80, 0x80}))
+	requireT.False(Contains([]byte{0x80, 0x80, 0x80, 0x80}))
+	requireT.False(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80}))
+	requireT.False(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80}))
+	requireT.False(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80}))
+	requireT.False(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x7F}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x7F}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x7F}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x7F}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x7F}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x7F}))
+	requireT.True(Contains([]byte{0x80, 0x7F}))
+	requireT.True(Contains([]byte{0x7F}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x7F, 0x80}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x7F, 0x80}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x7F, 0x80}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x80, 0x7F, 0x80}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x80, 0x7F, 0x80}))
+	requireT.True(Contains([]byte{0x80, 0x80, 0x7F, 0x80}))
+	requireT.True(Contains([]byte{0x80, 0x7F, 0x80}))
+	requireT.True(Contains([]byte{0x7F, 0x80}))
+}
+
+func TestPut(t *testing.T) {
+	requireT := require.New(t)
+	b := make([]byte, 9)
+
+	requireT.EqualValues(1, Put(b, 0))
+	requireT.Equal([]byte{0x00}, b[:1])
+
+	requireT.EqualValues(1, Put(b, 1))
+	requireT.Equal([]byte{0x01}, b[:1])
+
+	requireT.EqualValues(1, Put(b, max1Byte))
+	requireT.Equal([]byte{numberMask}, b[:1])
+
+	requireT.EqualValues(2, Put(b, max1Byte+1))
+	requireT.Equal([]byte{followMask, 0x01}, b[:2])
+
+	requireT.EqualValues(2, Put(b, max2Bytes))
+	requireT.Equal([]byte{0xFF, numberMask}, b[:2])
+
+	requireT.EqualValues(3, Put(b, max2Bytes+1))
+	requireT.Equal([]byte{followMask, followMask, 0x01}, b[:3])
+
+	requireT.EqualValues(3, Put(b, max3Bytes))
+	requireT.Equal([]byte{0xFF, 0xFF, numberMask}, b[:3])
+
+	requireT.EqualValues(4, Put(b, max3Bytes+1))
+	requireT.Equal([]byte{followMask, followMask, followMask, 0x01}, b[:4])
+
+	requireT.EqualValues(4, Put(b, max4Bytes))
+	requireT.Equal([]byte{0xFF, 0xFF, 0xFF, numberMask}, b[:4])
+
+	requireT.EqualValues(5, Put(b, max4Bytes+1))
+	requireT.Equal([]byte{followMask, followMask, followMask, followMask, 0x01}, b[:5])
+
+	requireT.EqualValues(5, Put(b, max5Bytes))
+	requireT.Equal([]byte{0xFF, 0xFF, 0xFF, 0xFF, numberMask}, b[:5])
+
+	requireT.EqualValues(6, Put(b, max5Bytes+1))
+	requireT.Equal([]byte{followMask, followMask, followMask, followMask, followMask, 0x01}, b[:6])
+
+	requireT.EqualValues(6, Put(b, max6Bytes))
+	requireT.Equal([]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, numberMask}, b[:6])
+
+	requireT.EqualValues(7, Put(b, max6Bytes+1))
+	requireT.Equal([]byte{followMask, followMask, followMask, followMask, followMask, followMask, 0x01}, b[:7])
+
+	requireT.EqualValues(7, Put(b, max7Bytes))
+	requireT.Equal([]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, numberMask}, b[:7])
+
+	requireT.EqualValues(8, Put(b, max7Bytes+1))
+	requireT.Equal([]byte{followMask, followMask, followMask, followMask, followMask, followMask, followMask, 0x01}, b[:8])
+
+	requireT.EqualValues(8, Put(b, max8Bytes))
+	requireT.Equal([]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, numberMask}, b[:8])
+
+	requireT.EqualValues(9, Put(b, max8Bytes+1))
+	requireT.Equal([]byte{followMask, followMask, followMask, followMask, followMask, followMask, followMask, followMask,
+		0x01}, b[:9])
+
+	requireT.EqualValues(9, Put(b, math.MaxUint64))
+	requireT.Equal([]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}, b[:9])
+}
+
+func TestParse(t *testing.T) {
+	requireT := require.New(t)
+
+	v, n := Parse([]byte{0x00})
+	requireT.EqualValues(1, n)
+	requireT.EqualValues(0, v)
+
+	v, n = Parse([]byte{0x00, 0x80})
+	requireT.EqualValues(1, n)
+	requireT.EqualValues(0, v)
+
+	v, n = Parse([]byte{0x01})
+	requireT.EqualValues(1, n)
+	requireT.EqualValues(1, v)
+
+	v, n = Parse([]byte{0x01, 0x80})
+	requireT.EqualValues(1, n)
+	requireT.EqualValues(1, v)
+
+	v, n = Parse([]byte{numberMask})
+	requireT.EqualValues(1, n)
+	requireT.EqualValues(max1Byte, v)
+
+	v, n = Parse([]byte{followMask, 0x01})
+	requireT.EqualValues(2, n)
+	requireT.EqualValues(max1Byte+1, v)
+
+	v, n = Parse([]byte{0xFF, numberMask})
+	requireT.EqualValues(2, n)
+	requireT.EqualValues(max2Bytes, v)
+
+	v, n = Parse([]byte{followMask, followMask, 0x01})
+	requireT.EqualValues(3, n)
+	requireT.EqualValues(max2Bytes+1, v)
+
+	v, n = Parse([]byte{0xFF, 0xFF, numberMask})
+	requireT.EqualValues(3, n)
+	requireT.EqualValues(max3Bytes, v)
+
+	v, n = Parse([]byte{followMask, followMask, followMask, 0x01})
+	requireT.EqualValues(4, n)
+	requireT.EqualValues(max3Bytes+1, v)
+
+	v, n = Parse([]byte{0xFF, 0xFF, 0xFF, numberMask})
+	requireT.EqualValues(4, n)
+	requireT.EqualValues(max4Bytes, v)
+
+	v, n = Parse([]byte{followMask, followMask, followMask, followMask, 0x01})
+	requireT.EqualValues(5, n)
+	requireT.EqualValues(max4Bytes+1, v)
+
+	v, n = Parse([]byte{0xFF, 0xFF, 0xFF, 0xFF, numberMask})
+	requireT.EqualValues(5, n)
+	requireT.EqualValues(max5Bytes, v)
+
+	v, n = Parse([]byte{followMask, followMask, followMask, followMask, followMask, 0x01})
+	requireT.EqualValues(6, n)
+	requireT.EqualValues(max5Bytes+1, v)
+
+	v, n = Parse([]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, numberMask})
+	requireT.EqualValues(6, n)
+	requireT.EqualValues(max6Bytes, v)
+
+	v, n = Parse([]byte{followMask, followMask, followMask, followMask, followMask, followMask, 0x01})
+	requireT.EqualValues(7, n)
+	requireT.EqualValues(max6Bytes+1, v)
+
+	v, n = Parse([]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, numberMask})
+	requireT.EqualValues(7, n)
+	requireT.EqualValues(max7Bytes, v)
+
+	v, n = Parse([]byte{followMask, followMask, followMask, followMask, followMask, followMask, followMask, 0x01})
+	requireT.EqualValues(8, n)
+	requireT.EqualValues(max7Bytes+1, v)
+
+	v, n = Parse([]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, numberMask})
+	requireT.EqualValues(8, n)
+	requireT.EqualValues(max8Bytes, v)
+
+	v, n = Parse([]byte{followMask, followMask, followMask, followMask, followMask, followMask, followMask, followMask,
+		0x01})
+	requireT.EqualValues(9, n)
+	requireT.EqualValues(max8Bytes+1, v)
+
+	v, n = Parse([]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF})
+	requireT.EqualValues(9, n)
+	requireT.EqualValues(uint64(math.MaxUint64), v)
+}
